3fb51a724967382935b7c37f6b1f660966c34152
Andreil <guff-0000@mail.ru>
2024-03-09 17:45:10 +0300

    привет

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Menu
+// Menu
+
+function openModal() {
+   document.getElementById("modal").style.top = "-20px";
+}
+
+function closeModal() {
+   document.getElementById("modal").style.top = "-460px";
+}
+
